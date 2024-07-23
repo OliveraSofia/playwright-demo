74445f98ea7959794edf5c4097a04d26a9b2d7ff
Sofia Olivera <olivera.sofia@gmail.com>
2024-07-23 16:58:11 -0300

    productInfo details

--- pages/Product-page.js
+++ pages/Product-page.js
@@ -6,9 +6,12 @@ export class ProductPage {
 
     constructor(page) {
         this.page = page;
-
-
-        
+        this.productInformation = page.locator('.product-information')
+        this.productQuantityLabel = this.productInformation.locator('label', {hasText : 'Quantity:'})
+        this.productAvailabilityLabel = this.productInformation.locator('p', {hasText : 'Availability:'})
+        this.productConditionLabel = this.productInformation.locator('p', {hasText : 'Condition:'})
+        this.productBrandLabel = this.productInformation.locator('p', {hasText : 'Brand:'})
+        this.productQuantityTextbox = page.locator('#quantity')
       };
 
       
@@ -16,5 +19,14 @@ export class ProductPage {
     await this.page.goto(productUrl);
   };
       
+  async productInfo() {
 
-}
+    await expect(this.productInformation).toBeVisible()
+    await expect(this.productQuantityLabel).toBeVisible()
+    await expect(this.productAvailabilityLabel).toBeVisible()
+    await expect(this.productConditionLabel).toBeVisible()
+    await expect(this.productBrandLabel).toBeVisible()
+    await expect(this.productQuantityTextbox).toBeEnabled()
+  };
+
+  };
--- tests/product-page.spec.js
+++ tests/product-page.spec.js
@@ -1,6 +1,13 @@
+import { test } from '@playwright/test';
 import { ProductPage } from '../pages/Product-page';
 
 test.beforeEach('Open view product page', async ({page}) => {
     const productPage = new ProductPage(page);
     await productPage.goto()
+  });
+
+
+  test('Verif Product Info is Visible', async ({page}) => {
+    const productPage = new ProductPage(page);
+    await productPage.productInfo()
   });
